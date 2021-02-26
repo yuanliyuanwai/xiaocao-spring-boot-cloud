9f309e1b6ede14d6e50dd87e051bdd1d4bb0defd
yuanliyuanwai <wanzhengchong@qq.com>
2021-02-26 14:53:10 +0800

    spring-boot启动时初始化资源

---
+++ spring-boot2/xiaocao-spring-boot2/spring-boot-commandLineRunner/src/main/java/top/wanzc/runner/ExceptionRunner.java
@@ -0,0 +1,14 @@
+package top.wanzc.runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ExceptionRunner implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+    	// throw new RuntimeException();
+    	// 在spring初始化线程中同步进行初始化，如果报错spring启动异常
+    }
+}
